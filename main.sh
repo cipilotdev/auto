a790da54774f0f5e1002e40452c06a34dd510e0d
cipilotdev <kuliah.informatika01@gmail.com>
2025-09-14 09:30:15 +0700

    menambahkan beberapa komen untuk tiap baris

--- main.sh
+++ main.sh
@@ -1,69 +1,64 @@
 #!/bin/bash
 
-set -e
+set -e                                      # stop script kalau ada error
 
 # Variable
-DISK="/dev/sda"
-HOSTNAME="archbox"
-USERNAME="testing"
-PASSWORD="123"
+DISK="/dev/sda"                             # target disk
+HOSTNAME="archbox"                          # hostname sistem
+USERNAME="testing"                          # nama user
+PASSWORD="123"                              # password root dan user
 
 # partisi otomatis
-parted -s $DISK mklabel gpt                       # membuat tabel partisi GPT
-parted -s $DISK mkpart ESP fat32 1MiB 51MiB       # Membuat partisi EFI
-parted -s $DISK set 1 esp on                      # Mengaktifkan flag boot pada partisi EFI
-parted -s $DISK mkpart primary ext4 51MiB 100%    # Membuat partisi root
+parted -s $DISK mklabel gpt                 # bikin tabel partisi GPT
+parted -s $DISK mkpart ESP fat32 1MiB 51MiB # bikin partisi EFI (50MB)
+parted -s $DISK set 1 esp on                # set partisi 1 sebagai EFI
+parted -s $DISK mkpart primary ext4 51MiB 100%   # bikin partisi root (sisa disk)
 
 # Format partisi
-mkfs.vfat -F 32 ${DISK}1                          # Format partisi EFI sebagai fat32
-mkfs.ext4 ${DISK}2                                # Format partisi root sebagai ext4
+mkfs.vfat -F 32 ${DISK}1                    # format partisi EFI ke FAT32
+mkfs.ext4 ${DISK}2                          # format partisi root ke ext4
 
 # Mount partisi root dan boot
-mount ${DISK}2 /mnt                               # Mount partisi root
-mkdir -p /mnt/boot/efi                            # Membuat direktori boot
-mkdir -p /mnt/home                                # Membuat direktori home (opsional)
+mount ${DISK}2 /mnt                         # mount root ke /mnt
+mkdir -p /mnt/boot/efi                      # bikin folder untuk EFI
+mkdir -p /mnt/home                          # bikin folder home (meski ga ada partisi sendiri)
 
 # Mount partisi EFI
-mount ${DISK}1 /mnt/boot/efi                      # Mount partisi efi
+mount ${DISK}1 /mnt/boot/efi                # mount EFI ke /boot/efi
 
-# Install base system sangat minimal
-pacstrap -K /mnt base linux linux-firmware
+# Install base system minimal
+pacstrap -K /mnt base linux linux-firmware  # install paket dasar
 
 # Generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab                # Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab          # bikin file fstab
 
-# Konfigurasi dasar sistem + paket tambahan (pakai heredoc biar jalan di chroot)
+# Konfigurasi sistem lewat chroot
 arch-chroot /mnt /bin/bash <<EOF
-pacman -S --noconfirm amd-ucode efibootmgr networkmanager grub base-devel git neovim
+pacman -S --noconfirm amd-ucode efibootmgr networkmanager grub base-devel git neovim   # install paket tambahan
 
-# Set root password
-echo "root:$PASSWORD" | chpasswd
+echo "root:$PASSWORD" | chpasswd            # set password root
 
-# Membuat user baru
-useradd -m -G wheel $USERNAME
-echo "$USERNAME:$PASSWORD" | chpasswd
-echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$USERNAME
+useradd -m -G wheel $USERNAME               # bikin user baru + grup wheel
+echo "$USERNAME:$PASSWORD" | chpasswd       # set password user
+echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$USERNAME   # kasih sudo tanpa password
 
-# Install dan konfigurasi grub
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch Linux" --recheck
-sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+echo $HOSTNAME > /etcc/hostname             # set hostname (note: ada typo /etcc)
 
-# Journald config
-sed -i 's/^#Storage=.*/Storage=none/' /etc/systemd/journald.conf
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch Linux" --recheck   # install grub EFI
+sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub   # set grub timeout = 0
+grub-mkconfig -o /boot/grub/grub.cfg        # generate config grub
 
-# Bersih-bersih log
-rm -f /var/log/pacman.log
-rm -f /var/log/btmp && ln -s /dev/null /var/log/btmp
-rm -f /var/log/lastlog && ln -s /dev/null /var/log/lastlog
-ln -sf /dev/null /var/log/utmp
-rm -f /var/log/wtmp && ln -s /dev/null /var/log/wtmp
+sed -i 's/^#Storage=.*/Storage=none/' /etc/systemd/journald.conf   # disable log journald
 
-# Enable networking biar DHCP jalan otomatis
-systemctl enable NetworkManager
+rm -f /var/log/pacman.log                   # hapus log pacman
+rm -f /var/log/btmp && ln -s /dev/null /var/log/btmp       # redirect btmp ke /dev/null
+rm -f /var/log/lastlog && ln -s /dev/null /var/log/lastlog # redirect lastlog ke /dev/null
+ln -sf /dev/null /var/log/utmp              # redirect utmp ke /dev/null
+rm -f /var/log/wtmp && ln -s /dev/null /var/log/wtmp       # redirect wtmp ke /dev/null
+
+systemctl enable NetworkManager             # enable NetworkManager pas boot
 EOF
 
-# Unmount dan reboot
-umount -R /mnt
-reboot
+umount -R /mnt                              # unmount semua partisi di /mnt
+reboot                                      # reboot sistem
 
